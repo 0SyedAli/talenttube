bd84676f44b3c7340eb7e827acc27e10381c96dc
0SyedAli <sa8449006@gmail.com>
2024-04-17 05:38:07 +0500

    changes

---
+++ blog-detail.php
@@ -0,0 +1,140 @@
+<?php
+$page = 'Home';
+$pageDesc = '""';
+include 'includes/header1.php';
+$page = 'home';
+?>
+
+
+<!-- blog posts uppper start -->
+<section class='blogs-pg'>
+    <section class="blogs-pg-upper">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class='text-left'>
+                        <h1>Recent blog posts</h1>
+                        <p>Discover all about fashion blog and article</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-12 col-lg-6">
+                    <div class='blog-up-right'>
+
+                        <img src="images/blogpg1.png" alt="blogpg1">
+                        <div>
+                            <h3>
+                                2 min reads
+                            </h3>
+                            <h2>
+                                Blog title goes here... Blog title goes here... Blog title goes here... Blog title goes
+                                here...
+                            </h2>
+                            <p>
+                                In today's hyper-competitive hiring landscape, simply posting an open position is no longer enough. You need to maximize your employer brand visibility to attract prime talent before your competitors do. TalentTube offers cutting-edge tools to expand your reach and get on the radar of high-caliber candidates. Our intelligent job distribution pushes your openings far and wide across the top online talent platforms. Strategically targeted to relevant job seekers, your positions are seen by thousands of qualified prospects across the U.S. 
+                            </p>
+                            <p>
+                                But we go beyond job postings to boost brand awareness. Unlike many of the best job posting sites in the USA, our employer profile pages become talent hub pages, attracting passive candidates to learn more about your company and culture. Share authentic employee content that brings your brand personality to life. TalentTube also optimizes your discoverability in candidate searches. Our SEO best practices enhance your visibility in relevant online searches. We analyze the top keyword terms job seekers use to find companies like yours. This allows us to fine tune your branding and content to attract those searching for their next great opportunity
+                            </p>
+                            <p>
+                                In today's hyper-competitive hiring landscape, simply posting an open position is no longer enough. You need to maximize your employer brand visibility to attract prime talent before your competitors do. TalentTube offers cutting-edge tools to expand your reach and get on the radar of high-caliber candidates. Our intelligent job distribution pushes your openings far and wide across the top online talent platforms. Strategically targeted to relevant job seekers, your positions are seen by thousands of qualified prospects across the U.S. 
+                            </p>
+                            <p>
+                                But we go beyond job postings to boost brand awareness. Unlike many of the best job posting sites in the USA, our employer profile pages become talent hub pages, attracting passive candidates to learn more about your company and culture. Share authentic employee content that brings your brand personality to life. TalentTube also optimizes your discoverability in candidate searches. Our SEO best practices enhance your visibility in relevant online searches. We analyze the top keyword terms job seekers use to find companies like yours. This allows us to fine tune your branding and content to attract those searching for their next great opportunity
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-5">
+                    <div class='blog-up-left'>
+                        <div>
+                            <img src="images/blogpg2.png" alt="blogpg2">
+                            <div>
+                                <h3>2 min reads</h3>
+                                <h4>Blog title goes here... Blog title goes here </h4>
+                                <p>
+                                    Discover the latest fashion trends, essential pieces, and styling tips to keep your
+                                    style on point for the current season.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="images/blogpg3.png" alt="blogpg2">
+                            <div>
+                                <h3>2 min reads</h3>
+                                <h4>Blog title goes here Blog title goes here... </h4>
+                                <p>
+                                    Discover the latest fashion trends, essential pieces, and styling tips to keep your
+                                    style on point for the current season.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="images/blogpg4.png" alt="blogpg2">
+                            <div>
+                                <h3>2 min reads</h3>
+                                <h4>Blog title goes here Blog title goes here... </h4>
+                                <p>
+                                    Discover the latest fashion trends, essential pieces, and styling tips to keep your
+                                    style on point for the current season.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="images/blogpg4.png" alt="blogpg2">
+                            <div>
+                                <h3>2 min reads</h3>
+                                <h4>Blog title goes here Blog title goes here... </h4>
+                                <p>
+                                    Discover the latest fashion trends, essential pieces, and styling tips to keep your
+                                    style on point for the current season.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="images/blogpg4.png" alt="blogpg2">
+                            <div>
+                                <h3>2 min reads</h3>
+                                <h4>Blog title goes here Blog title goes here... </h4>
+                                <p>
+                                    Discover the latest fashion trends, essential pieces, and styling tips to keep your
+                                    style on point for the current season.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="images/blogpg4.png" alt="blogpg2">
+                            <div>
+                                <h3>2 min reads</h3>
+                                <h4>Blog title goes here Blog title goes here... </h4>
+                                <p>
+                                    Discover the latest fashion trends, essential pieces, and styling tips to keep your
+                                    style on point for the current season.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="images/blogpg4.png" alt="blogpg2">
+                            <div>
+                                <h3>2 min reads</h3>
+                                <h4>Blog title goes here Blog title goes here... </h4>
+                                <p>
+                                    Discover the latest fashion trends, essential pieces, and styling tips to keep your
+                                    style on point for the current season.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
+
+<!-- blog posts uppper end -->
+
+<?php
+include 'includes/footer.php';
+$page = 'home';
+?>
